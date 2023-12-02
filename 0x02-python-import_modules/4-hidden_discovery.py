5faf8114212c7e3486fd3c4f98137dee4b68e0ce
Dan01-M <www.dan56mateo@gmail.com>
2023-12-01 17:32:05 -0800

    pyh

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#check if the script is being run as the main program above 
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
