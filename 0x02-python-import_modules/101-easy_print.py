fc8797e82c05bf564e1ce071856a689ff66db117
Dan01-M <www.dan56mateo@gmail.com>
2023-12-01 18:06:01 -0800

    jbv

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# program that prints #pythoniscool, followed by a new line, in the standard output.
-__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
 
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
