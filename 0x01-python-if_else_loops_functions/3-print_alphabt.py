86f9641197a6bac300354c8b0d1e6e839ff45301
Dan01-M <www.dan56mateo@gmail.com>
2023-12-05 22:51:48 -0800

    3-print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+# program that prints the ASCII alphabet, in lowercase
 for i in range(97, 123):
     if (i == 101) or (i == 113):
         continue
     print(chr(i).format(), end="")
-
