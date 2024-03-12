de26ce9d31c8c42b15872dd27fbfa726a49df233
Dan01-M <www.dan56mateo@gmail.com>
2024-03-12 02:33:47 +0000

    wakes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('No Argument found');
+  console.log('No argument found');
 } else {
   console.log('Arguments found');
 }
