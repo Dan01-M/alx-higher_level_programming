409d673dbf1ad5979b7de25275b0912fe032d26e
Dan01-M <www.dan56mateo@gmail.com>
2024-03-14 10:06:21 +0000

    js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,10 +5,13 @@ const totalist = Object.entries(dict);
 const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const j in valsUniq) {
+for (const j in valsUniq) 
+{
   const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
+  for (const k in totalist) 
+{
+    if (totalist[k][1] === valsUniq[j]) 
+{
       list.unshift(totalist[k][0]);
     }
   }
