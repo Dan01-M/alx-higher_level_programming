7f5807ed6b6c7e82815cc4e2c6c6e85736657542
Dan01-M <www.dan56mateo@gmail.com>
2024-03-20 00:17:34 +0000

    js

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write a script that deletes all State objects with a name containing the letter """
+""" Write a script that deletes all State objects with a name containing the letter"""
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
