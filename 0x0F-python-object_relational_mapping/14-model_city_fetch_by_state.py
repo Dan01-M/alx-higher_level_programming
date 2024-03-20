e1c69872f6201231fdc3ceac21a815f2ddea86bb
Dan01-M <www.dan56mateo@gmail.com>
2024-03-20 00:41:20 +0000

    hd

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the datae allocations """
+""" prints the State object with the name passed as argument from the datae allocations."""
 import sys
 from model_state import Base, State
 from model_city import City
